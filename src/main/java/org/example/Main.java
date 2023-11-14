5b288ba929526cb7248d28c50a3f3af3d7d673aa
Victor Colin Amador <vicolina@microsoft.com>
2023-11-13 17:52:16 -0800

    Updated the app to use models in the generated open-ai library.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,13 @@
 package org.example;
 
 import com.generic.core.credential.KeyCredential;
-import com.generic.core.http.Response;
-import com.generic.core.implementation.http.serializer.DefaultJsonSerializer;
-import com.generic.core.models.BinaryData;
 import com.openai.OpenAIClient;
 import com.openai.OpenAIClientBuilder;
+import com.openai.models.ChatCompletionRequestMessage;
+import com.openai.models.ChatCompletionRequestMessageRole;
+import com.openai.models.CreateChatCompletionRequest;
+import com.openai.models.CreateChatCompletionRequestModel;
+import com.openai.models.CreateChatCompletionResponse;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,20 +18,25 @@ public class Main {
             .credential(new KeyCredential(System.getenv("OPENAI_SECRET_KEY")))
             .buildClient();
 
-        List<ChatCompletionParameters.Message> messages = new ArrayList<>();
-        messages.add(new ChatCompletionParameters.Message("system", "You are a helpful assistant."));
-        messages.add(new ChatCompletionParameters.Message("user", "Who won the world series in 2020?"));
-        messages.add(new ChatCompletionParameters.Message("user", "The Los Angeles Dodgers won the World Series in 2020."));
-        messages.add(new ChatCompletionParameters.Message("user", "Where was it played?"));
+        List<ChatCompletionRequestMessage> messages = new ArrayList<>();
+        messages.add(new ChatCompletionRequestMessage(ChatCompletionRequestMessageRole.SYSTEM,
+            "You are a helpful assistant."));
+        messages.add(new ChatCompletionRequestMessage(ChatCompletionRequestMessageRole.USER,
+            "Who won the world series in 2020?"));
+        messages.add(new ChatCompletionRequestMessage(ChatCompletionRequestMessageRole.ASSISTANT,
+            "The Los Angeles Dodgers won the World Series in 2020."));
+        messages.add(new ChatCompletionRequestMessage(ChatCompletionRequestMessageRole.USER,
+            "Where was it played?"));
 
-        ChatCompletionParameters parameters = new ChatCompletionParameters("gpt-3.5-turbo", messages);
+        CreateChatCompletionRequest createChatCompletionRequest =
+            new CreateChatCompletionRequest(CreateChatCompletionRequestModel.GPT_3_5_TURBO, messages);
 
-        Response<BinaryData> chatCompletionResponse =
-            openAIClient.createChatCompletionWithResponse(
-                BinaryData.fromBytes(
-                    new DefaultJsonSerializer().serializeToBytes(parameters)), null);
+        CreateChatCompletionResponse createChatCompletionResponse =
+            openAIClient.createChatCompletion(createChatCompletionRequest);
 
-        System.out.println("Request completed with status code: " + chatCompletionResponse.getStatusCode());
-        System.out.println("Response value: " + chatCompletionResponse.getValue().toString());
+        System.out.println("Retrieved the following choices for a chat completion:");
+
+        createChatCompletionResponse.getChoices()
+            .forEach(choice -> System.out.println(choice.getMessage().getContent()));
     }
 }
